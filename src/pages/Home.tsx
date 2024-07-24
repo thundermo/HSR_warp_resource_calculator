7dc3eb2f3ad0478e66aca81be74d64679ed94c18
ThunderMo <113253460+thundermo@users.noreply.github.com>
2024-07-24 13:18:29 +0800

    deleted useless code (#3)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,7 @@
 import { useState } from "react";
-import Alert from "../components/Alert";
-import Button from "../components/Button";
 import ResourseCalculator from "../components/ResourceCalculator";
 
 function Home() {
-  let items = ["AAAAAAAAA", "BBBBBBBBB", "CCCCCCCCC", "DDDDDDDDD", "EEEEEEEEE"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   const [alertVisible, setAlertVisible] = useState(false);
 
   return (
