faa4f973dcd047c9a559eda677615aebfa9bc2a1
ThunderMo <113253460+thundermo@users.noreply.github.com>
2024-07-24 20:58:28 +0800

    update (#5)

---
+++ src/hooks/useFormInputHandler.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+const useInputHandler = (initialValue = "0") => {
+  const [value, setValue] = useState(initialValue);
+
+  const handleChange = (newValue: string) => {
+    setValue(newValue);
+  };
+
+  const handleFocus = () => {
+    if (value === "0") setValue("");
+  };
+
+  const handleBlur = () => {
+    if (value === "") setValue("0");
+  };
+
+  return {
+    value,
+    handleChange,
+    handleFocus,
+    handleBlur,
+  };
+};
+
+export default useInputHandler;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,10 +5,8 @@ import "./Home.css";
 function Home() {
   return (
     <>
-      <div className="border container vh-100 d-flex align-items-center justify-content-center">
-        <div className="border mx-3 mt-5 bg-light">
-          <ResourseCalculator />
-        </div>
+      <div className="container calculator">
+        <ResourseCalculator />
       </div>
     </>
   );
