f458c85f129c39953fab5d6442b81ed029d16d44
ThunderMo <113253460+thundermo@users.noreply.github.com>
2024-07-25 23:06:54 +0800

    add deploy workflow (#7)

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import "./App.css";
 import "./main.css";
 import ErrorPage from "./pages/ErrorPage";
